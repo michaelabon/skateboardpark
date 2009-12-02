0f9740009d865adac2fb516baa93336a68515233
Rami Hesham <ramihg@gmail.com>
2009-12-02 04:42:15 +0000

    Merged scene.cs

--- SkatePark/BlockControl.cs
+++ SkatePark/BlockControl.cs
@@ -7,9 +7,6 @@ namespace SkatePark
 {
     public partial class Scene
     {
-        private void OnBlockSelected(int blockNum)
-        {
-            Console.WriteLine("Block number: " + blockNum + " selected.");
-        }
+        
     }
 }
